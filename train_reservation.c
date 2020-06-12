2a5fea6138c1088c6eec28928e9a8e0bb493d3f3
Aviral Omar <aviralomar0301@gmail.com>
2020-06-12 18:06:55 +0530

    Added enquiry system

--- train_reservation.c
+++ train_reservation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 typedef struct train
@@ -15,6 +16,7 @@ typedef struct ticket
     unsigned long int pnr;
     char name[50];
     unsigned int number;
+    unsigned int cost;
 } TICKET;
 
 TRAIN trains[100];
@@ -24,18 +26,76 @@ int trainCount = 0;
 int ticketCount = 0;
 
 void welcome();
+void enquiry();
 void loadData();
 void saveData();
 void showTrains();
 
 int main()
 {
-    // system("clear");
+    system("clear");
     loadData();
     welcome();
     saveData();
-    // system("clear");
+    system("clear");
     return 0;
+}
+
+void welcome()
+{
+    char choice;
+    printf("\tWelcome to Shramik Sahyog Kendra\n\n");
+    printf("\tHow can we help you today?\n\n");
+    printf("1. Enquiry\n");
+    printf("2. Booking\n");
+    printf("3. Cancellation\n");
+    printf("0. Exit\n\n");
+    printf("Choose your desired operation: ");
+    choice = fgetc(stdin);
+    //this clears the input buffer
+    while (fgetc(stdin) != '\n')
+        ;
+    if (choice == '0')
+    {
+        return;
+    }
+    else if (choice == '1')
+    {
+        system("clear");
+        enquiry();
+    }
+    system("clear");
+    welcome();
+}
+
+void enquiry()
+{
+    char sr[30];
+    printf("Enter Source: ");
+    fgets(sr, 28, stdin);
+    sr[strlen(sr) - 1] = '\0';
+    if (strcmp(sr, "Delhi") == 0)
+    {
+        char dt[30];
+        printf("Enter Destination: ");
+        fgets(dt, 28, stdin);
+        dt[strlen(dt) - 1] = '\0';
+        for (int i = 0; i < trainCount; i++)
+        {
+            if (strcmp(dt, trains[i].destination) == 0)
+            {
+                printf("\n\nNumber of seats available: %hu\n\n", trains[i].seatsLeft);
+                printf("Press ENTER to continue...");
+                while (fgetc(stdin) != '\n')
+                    ;
+                return;
+            }
+        }
+    }
+    printf("\n\nNo trains are available for given source and destination!\n\n");
+    printf("Press ENTER to continue...");
+    while (fgetc(stdin) != '\n')
+        ;
 }
 
 void saveData()
@@ -117,29 +177,5 @@ void showTrains()
     {
         TRAIN tr = trains[i];
         printf("%d.\t%5s%*s%-8s%-20s%-12hu\n", i + 1, tr.number, 7, "", tr.source, tr.destination, tr.seatsLeft);
-    }
-}
-
-void welcome()
-{
-    char choice;
-    printf("\tWelcome to Shramik Sahyog Kendra\n\n");
-    printf("\tHow can we help you today?\n\n");
-    printf("1. Enquiry\n");
-    printf("2. Booking\n");
-    printf("3. Cancellation\n");
-    printf("0. Exit\n\n");
-    printf("Choose your desired operation: ");
-    choice = fgetc(stdin);
-    //this clears the input buffer
-    while (fgetc(stdin) != '\n')
-        ;
-    if (choice == '0')
-    {
-        return;
-    }
-    else if (choice == '1')
-    {
-        showTrains();
     }
 }
